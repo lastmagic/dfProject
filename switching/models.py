9d71eee57d82ce9579231013390431c92fe04bb3
YouHuiDong <youheedong@gmail.com>
2018-02-25 18:05:11 +0900

    models에 user객체추가, admin.site에 User추가

--- switching/models.py
+++ switching/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    serverList = (
+        ('bakal', 'bakal'),
+        ('cain','cain'),
+        ('casillas', 'casillas'),
+        ('diregie','diregie'),
+        ('hilder', 'hiler'),
+        ('prey', 'prey'),
+        ('siroco', 'siroco'),
+    )
+    characterName = models.CharField(max_length=30)
+    serverName = models.CharField(max_length = 10,choices = serverList, default="cain")
+    def __str__(self):
+        return "name:"+self.characterName + "server:" + self.serverName
